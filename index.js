9e6c0adc363c0fccea0b6676394faf5241823fa7
Marco Fiset <marco@rumandcode.io>
2023-05-17 08:27:54 -0400

    Change configuration source from env vars to toml file

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const axios = require('axios');
 const { Command } = require('commander');
-const program = new Command();
-const dotenv = require('dotenv');
+const fs = require('fs');
+const toml = require('toml');
 
-dotenv.config();
+const program = new Command();
+
+if (!fs.existsSync('./config.toml')) {
+  console.error('Error: config.toml not found');
+  process.exit(1);
+}
+
+const file = fs.readFileSync('./config.toml', 'utf-8');
+config = toml.parse(file);
+
 program.version('1.0.0');
 program
   .action(async () => {
-    const owner = process.env.OWNER;
-    const team = process.env.TEAM_SLUG;
-    const token = process.env.TOKEN;
-    const excludeBots = (process.env.EXCLUDE_BOTS && process.env.EXCLUDE_BOTS != 'false') || true;
+    const { owner, team, token, exclude_bots: excludeBots } = config;
     const headers = {
       'Authorization': `Bearer ${token}`,
       'Accept': 'application/vnd.github+json',
@@ -22,7 +28,7 @@ program
     console.log('\x1b[33m%s\x1b[0m%s', "Team: ", team);
     console.log("\nFetching repositories...");
 
-    const response = await axios.get(`https://api.github.com/orgs/${owner}/repos?per_page=100`, {headers});
+    const response = await axios.get(`https://api.github.com/orgs/${owner}/repos?per_page=100`, { headers });
     if (response.status !== 200) {
       console.error(`Error: ${response.status} ${response.statusText}`);
       process.exit(1);
@@ -31,7 +37,7 @@ program
     console.log("Found", repoNames.length, "repositories for organization", owner);
 
     const filterByTeam = async (repoName) => {
-      const response = await axios.get(`https://api.github.com/repos/${owner}/${repoName}/teams`, {headers});
+      const response = await axios.get(`https://api.github.com/repos/${owner}/${repoName}/teams`, { headers });
       if (response.status !== 200) {
         console.error(`Error: ${response.status} ${response.statusText}`);
         process.exit(1);
@@ -46,24 +52,25 @@ program
     }
 
     const getOpenPullRequests = async (repoName) => {
-      const response = await axios.get(`https://api.github.com/repos/${owner}/${repoName}/pulls?state=open`, {headers});
+      const response = await axios.get(`https://api.github.com/repos/${owner}/${repoName}/pulls?state=open`, { headers });
       if (response.status !== 200) {
         console.error(`Error: ${response.status} ${response.statusText}`);
         process.exit(1);
       }
       return response.data
-      .filter((pr) => {
-        if (excludeBots) {
-          return pr.user.type !== 'Bot';
-        } else {
-          return true;
-        }
-      })
+        .filter((pr) => {
+          if (excludeBots) {
+            return pr.user.type !== 'Bot';
+          } else {
+            return true;
+          }
+        })
     }
 
-    const enhancePullRequestWithReview = async (pullRequest) => {;
+    const enhancePullRequestWithReview = async (pullRequest) => {
+      ;
       const { number, head: { repo: { name } } } = pullRequest;
-      const response = await axios.get(`https://api.github.com/repos/${owner}/${name}/pulls/${number}/reviews`, {headers});
+      const response = await axios.get(`https://api.github.com/repos/${owner}/${name}/pulls/${number}/reviews`, { headers });
       if (response.status !== 200) {
         console.error(`Error: ${response.status} ${response.statusText}`);
         process.exit(1);
@@ -74,30 +81,30 @@ program
     }
 
     filterAsync(repoNames, filterByTeam)
-    .then(async (repos) => {
-      console.log("Found", repos.length, "repositories for team", team);
-      const result = await Promise.all(repos.map(getOpenPullRequests));
-      const openPullRequests = result.flat();
-      console.log("Found", openPullRequests.length, "open pull requests in these repos");
-      const openPullRequestsWithReviews = await Promise.all(openPullRequests.map(enhancePullRequestWithReview));
-      console.log("Getting reviews for these pull requests...");
-      console.log("\n");
-      openPullRequestsWithReviews
-      .forEach((pr) => {
-        if (pr.draft) {
-          console.log('\x1b[36m%s\x1b[0m (Draft)', pr.title);
-        } else if (pr.reviewStates.includes('APPROVED')) {
-          console.log('\x1b[32m%s\x1b[0m', pr.title); // green
-        } else {
-          console.log('\x1b[33m%s\x1b[0m', pr.title); // yellow
-        }
-        console.log(pr._links.html.href);
-        if (pr.reviewStates.length !== 0) {
-          console.log("Review States:", pr.reviewStates.join(', '));
-        }
+      .then(async (repos) => {
+        console.log("Found", repos.length, "repositories for team", team);
+        const result = await Promise.all(repos.map(getOpenPullRequests));
+        const openPullRequests = result.flat();
+        console.log("Found", openPullRequests.length, "open pull requests in these repos");
+        const openPullRequestsWithReviews = await Promise.all(openPullRequests.map(enhancePullRequestWithReview));
+        console.log("Getting reviews for these pull requests...");
         console.log("\n");
+        openPullRequestsWithReviews
+          .forEach((pr) => {
+            if (pr.draft) {
+              console.log('\x1b[36m%s\x1b[0m (Draft)', pr.title);
+            } else if (pr.reviewStates.includes('APPROVED')) {
+              console.log('\x1b[32m%s\x1b[0m', pr.title); // green
+            } else {
+              console.log('\x1b[33m%s\x1b[0m', pr.title); // yellow
+            }
+            console.log(pr._links.html.href);
+            if (pr.reviewStates.length !== 0) {
+              console.log("Review States:", pr.reviewStates.join(', '));
+            }
+            console.log("\n");
+          });
       });
-    });
   });
 
 program.parse(process.argv);
